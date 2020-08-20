f07790bb5204842ce9a8a5bf11a446665776c41e
Atanas Atanasov <atanas4537@gmail.com>
2020-08-20 18:31:16 +0300

    refactored cookie parsing in authorization headers

--- Client/myblog.client/src/services/articles.js
+++ Client/myblog.client/src/services/articles.js
@@ -24,12 +24,11 @@ const getArticlesCount = async (onSuccess, onFailure) => {
 };
 
 const getAllArticlesByCurrentUser = async (page, onSuccess, onFailure) => {
-	const cookie = document.cookie.split("=");
 	await crud.get(
 		routes.GET_ALL_ARTICLES_BY_CURRENT_USER(page),
 		{
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${cookie[1]}`,
+			Authorization: document.cookie.replace("=", " "),
 		},
 		onSuccess,
 		onFailure
@@ -42,7 +41,7 @@ const getArticlesCountByCurrentUser = async (onSuccess, onFailure) => {
 		routes.GET_ALL_ARTICLES_COUNT_BY_CURRENT_USER,
 		{
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${cookie[1]}`,
+			Authorization: document.cookie.replace("=", " "),
 		},
 		onSuccess,
 		onFailure
@@ -61,14 +60,12 @@ const getArticleById = async (id, onSuccess, onFailure) => {
 };
 
 const createArticle = async (body, onSuccess, onFailure) => {
-	const cookie = document.cookie.split("=");
-
 	await crud.input(
 		routes.CREATE_ARTICLE,
 		"POST",
 		{
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${cookie[1]}`,
+			Authorization: document.cookie.replace("=", " "),
 		},
 		body,
 		onSuccess,
@@ -77,13 +74,12 @@ const createArticle = async (body, onSuccess, onFailure) => {
 };
 
 const editArticle = async (id, body, onSuccess, onFailure) => {
-	const cookie = document.cookie.split("=");
 	await crud.input(
 		routes.EDIT_ARTICLE_BY_ID(id),
 		"PUT",
 		{
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${cookie[1]}`,
+			Authorization: document.cookie.replace("=", " "),
 		},
 		body,
 		onSuccess,
