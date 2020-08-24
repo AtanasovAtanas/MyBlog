d9a9c36a841aec3ba7176e48e91274f0eff602cd
Atanas Atanasov <atanas4537@gmail.com>
2020-08-24 20:58:51 +0300

    Add processing of multiple cookies in cookieHelper.js. This closes #21.

--- Client/myblog.client/src/utils/cookieHelper.js
+++ Client/myblog.client/src/utils/cookieHelper.js
@@ -1,5 +1,20 @@
 const getCurrentCookie = () => {
-	return document.cookie.replace("Bearer=", "Bearer ");
+	let result = "";
+
+	const cookies = document.cookie.split("; ");
+
+	cookies.forEach((cookie) => {
+		const parts = cookie.split("=");
+		const name = parts[0];
+
+		if (name === "Bearer") {
+			const value = parts[1];
+			result = `Bearer ${value}`;
+			return;
+		}
+	});
+
+	return result ? result : "Bearer ";
 };
 
 export default getCurrentCookie;
