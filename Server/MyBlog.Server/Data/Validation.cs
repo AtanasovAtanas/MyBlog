79aee16ef66ab0cf6de14a7305f627740b9432c8
Atanas Atanasov <atanas4537@gmail.com>
2020-08-27 16:59:48 +0300

    add more data validation for input article

--- Server/MyBlog.Server/Data/Validation.cs
+++ Server/MyBlog.Server/Data/Validation.cs
@@ -4,7 +4,11 @@
     {
         public class Articles
         {
+            public const int TitleMinLength = 5;
+
             public const int TitleMaxLength = 60;
+
+            public const int ContentMinLength = 20;
         }
     }
 }
