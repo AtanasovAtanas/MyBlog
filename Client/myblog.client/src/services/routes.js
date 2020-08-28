62a59f76de384faffd6fcde2261b75a768000ec8
Atanas Atanasov <atanas4537@gmail.com>
2020-08-28 07:52:52 +0300

    Refactored services/routes by extracting base url by feature and remove duplicated routes. This closes #40.

--- Client/myblog.client/src/services/routes.js
+++ Client/myblog.client/src/services/routes.js
@@ -1,7 +1,12 @@
 const BASE_URL = "https://localhost:5001";
+const IDENTITY_BASE_URL = BASE_URL + "/Identity";
+const ARTICLES_BASE_URL = BASE_URL + "/Articles";
+const VOTES_BASE_URL = BASE_URL + "/Votes";
+const COMMENTS_BASE_URL = BASE_URL + "/Comments";
+const CATEGORIES_BASE_URL = BASE_URL + "/Categories";
 
 const getArticlesByCategoryName = (categoryName, page, filter) => {
-	let url = BASE_URL + "/Categories/" + categoryName;
+	let url = CATEGORIES_BASE_URL + "/" + categoryName;
 
 	if (page) {
 		url = url + `?page=${page}&`;
@@ -15,7 +20,7 @@ const getArticlesByCategoryName = (categoryName, page, filter) => {
 };
 
 const getArticlesCountByCategoryName = (categoryName, filter) => {
-	let url = BASE_URL + `/Categories/${categoryName}/Articles/Count`;
+	let url = CATEGORIES_BASE_URL + `/${categoryName}/Articles/Count`;
 
 	if (filter) {
 		url = url + `?filter=${filter}`;
@@ -24,41 +29,42 @@ const getArticlesCountByCategoryName = (categoryName, filter) => {
 	return url;
 };
 
+const byId = (base, id) => base + `/${id}`;
+
 export const IdentityRoutes = {
-	LOGIN: BASE_URL + "/Identity/Login",
-	REGISTER: BASE_URL + "/Identity/Register",
-	GET_IDENTITY_DETAILS: BASE_URL + "/Identity/",
+	LOGIN: IDENTITY_BASE_URL + "/Login",
+	REGISTER: IDENTITY_BASE_URL + "/Register",
+	GET_IDENTITY_DETAILS: IDENTITY_BASE_URL,
 };
 
 export const ArticleRoutes = {
-	GET_ARTICLE_BY_ID: (id) => BASE_URL + `/Articles/${id}`,
+	GET_ARTICLE_BY_ID: (id) => byId(ARTICLES_BASE_URL, id),
 	GET_ALL_ARTICLES_BY_CURRENT_USER: (page) =>
-		BASE_URL + `/Articles/Mine?page=${page ? page : 1}`,
-	GET_ALL_ARTICLES_COUNT_BY_CURRENT_USER: BASE_URL + "/Articles/Mine/Count",
-	CREATE_ARTICLE: BASE_URL + "/Articles",
-	EDIT_ARTICLE_BY_ID: (id) => BASE_URL + `/Articles/${id}`,
-	DELETE_ARTICLE_BY_ID: (id) => BASE_URL + `/Articles/${id}`,
+		ARTICLES_BASE_URL + `/Mine?page=${page ? page : 1}`,
+	GET_ALL_ARTICLES_COUNT_BY_CURRENT_USER: ARTICLES_BASE_URL + "/Mine/Count",
+	CREATE_ARTICLE: ARTICLES_BASE_URL,
+	EDIT_ARTICLE_BY_ID: (id) => byId(ARTICLES_BASE_URL, id),
+	DELETE_ARTICLE_BY_ID: (id) => byId(ARTICLES_BASE_URL, id),
+	GET_COMMENTS_BY_ARTICLE_ID: (articleId) =>
+		byId(ARTICLES_BASE_URL, articleId) + `/Comments`,
 };
 
 export const VoteRoutes = {
-	GET_VOTES_BY_ARTICLE_ID: (id) => BASE_URL + `/Votes/${id}`,
-	POST_VOTE_TO_ARTICLE_ID: BASE_URL + "/Votes",
+	GET_VOTES_BY_ARTICLE_ID: (id) => byId(VOTES_BASE_URL, id),
+	POST_VOTE_TO_ARTICLE_ID: VOTES_BASE_URL,
 	GET_USER_VOTE_TYPE_BY_ARTICLE_ID: (id) =>
-		BASE_URL + `/Votes?articleId=${id}`,
+		VOTES_BASE_URL + `?articleId=${id}`,
 };
 
 export const CommentRoutes = {
-	GET_COMMENTS_BY_ARTICLE_ID: (articleId) =>
-		BASE_URL + `/Articles/${articleId}/Comments`,
-	GET_REPLIES_BY_COMMENT_ID: (commentId) =>
-		BASE_URL + `/Comments/${commentId}`,
-	POST_COMMENT: BASE_URL + "/Comments",
-	DELETE_COMMENT_BY_ID: (id) => BASE_URL + `/Comments/${id}`,
-	UPDATE_COMMENT_BY_ID: (id) => BASE_URL + `/Comments/${id}`,
+	GET_REPLIES_BY_COMMENT_ID: (id) => byId(COMMENTS_BASE_URL, id),
+	POST_COMMENT: COMMENTS_BASE_URL,
+	DELETE_COMMENT_BY_ID: (id) => byId(COMMENTS_BASE_URL, id),
+	UPDATE_COMMENT_BY_ID: (id) => byId(COMMENTS_BASE_URL, id),
 };
 
 export const CategoryRoutes = {
-	GET_CATEGORIES: BASE_URL + "/Categories",
+	GET_CATEGORIES: CATEGORIES_BASE_URL,
 	GET_ARTICLES_BY_CATEGORY_NAME: (categoryName, page, filter) =>
 		getArticlesByCategoryName(categoryName, page, filter),
 	GET_ARTICLES_COUNT_BY_CATEGORY_NAME: (categoryName, filter) =>
