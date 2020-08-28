2fc137a89114c883b6010d738210f9c922b23774
Atanas Atanasov <atanas4537@gmail.com>
2020-08-28 12:41:26 +0300

    add footer component

--- Client/myblog.client/src/components/footer/index.js
+++ Client/myblog.client/src/components/footer/index.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./index.module.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon as FontAwesome } from "@fortawesome/react-fontawesome";
+import {
+	faFacebook,
+	faInstagram,
+	faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-	return <footer></footer>;
+	return (
+		<Navbar className={styles.footer} sticky="bottom">
+			<img
+				alt="logo"
+				src="/favicon.ico"
+				width="30"
+				height="30"
+				className="d-inline-block align-top"
+			/>
+			<Link to="/">My blog</Link>
+			<div className={styles.social}>
+				<span>
+					<Link to="#">
+						<FontAwesome icon={faFacebook} size="lg" />
+					</Link>
+				</span>
+				<span>
+					<Link to="#">
+						<FontAwesome icon={faInstagram} size="lg" />
+					</Link>
+				</span>
+				<span>
+					<Link to="#">
+						<FontAwesome icon={faTwitter} size="lg" />
+					</Link>
+				</span>
+			</div>
+			<div className={styles.copyright}>
+				&copy; 2020 My Blog. All rights reserved.
+			</div>
+		</Navbar>
+	);
 };
 
 export default Footer;
