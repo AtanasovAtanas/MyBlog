f267ca786d1869676ceff3eeca8ee77ee3f9a037
Atanas Atanasov <atanas4537@gmail.com>
2020-09-04 13:02:52 +0300

    fix stylecop warning

--- Server/MyBlog.Server/Features/Articles/Models/ArticleDetailsResponseModel.cs
+++ Server/MyBlog.Server/Features/Articles/Models/ArticleDetailsResponseModel.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace MyBlog.Server.Features.Articles.Models
+﻿namespace MyBlog.Server.Features.Articles.Models
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     using AutoMapper;
