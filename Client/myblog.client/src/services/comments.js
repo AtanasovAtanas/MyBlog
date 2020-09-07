f1184933d2c6698bdf7f999aea27507990760041
Atanas Atanasov <atanas4537@gmail.com>
2020-09-07 12:09:11 +0300

    remove obsolete service methods

--- Client/myblog.client/src/services/comments.js
+++ Client/myblog.client/src/services/comments.js
@@ -2,17 +2,6 @@ import { CommentRoutes } from "./routes";
 import crud from "./crud";
 import getCurrentCookie from "../utils/cookieHelper";
 
-const getRepliesByCommentId = async (id, onSuccess, onFailure) => {
-	await crud.get(
-		CommentRoutes.GET_REPLIES_BY_COMMENT_ID(id),
-		{
-			"Content-Type": "application/json",
-		},
-		onSuccess,
-		onFailure
-	);
-};
-
 const postReply = async (body, onSuccess, onFailure) => {
 	await crud.input(
 		CommentRoutes.POST_COMMENT,
@@ -50,7 +39,6 @@ const deleteComment = async (id, onSuccess, onFailure) => {
 };
 
 export default {
-	getRepliesByCommentId,
 	postReply,
 	deleteComment,
 	udpateComment,
